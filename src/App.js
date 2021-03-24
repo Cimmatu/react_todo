5f37f04378adf3089fb9148818f9620a9eac6f34
unknown <pashagurinovich@gmail.com>
2021-03-24 15:50:33 +0300

    adaptive form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-
+import {useState} from "react"
 import './App.css';
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello react</h1>
-    </div>
-  );
+const App = () => {
+    const [inputText, setInputText] = useState('')
+    const [todos, setTodos] = useState([])
+    return (
+        <div className="App">
+            <header>
+                <h1>Ed's Todo List</h1>
+            </header>
+            <Form inputText={inputText} setInputText={setInputText} todos={todos} setTodos={setTodos}/>
+            <TodoList />
+        </div>
+    )
 }
 
 export default App;
